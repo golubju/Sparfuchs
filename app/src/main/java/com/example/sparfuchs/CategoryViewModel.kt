015a43686c450811ec357aa9fa1de0d1dd51bb64
Myriel <myriel.sarx@outlook.de>
2025-03-04 17:05:19 +0100

    added category overview and CategoryEntity

---
+++ app/src/main/java/com/example/sparfuchs/CategoryViewModel.kt
@@ -0,0 +1,33 @@
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.sparfuchs.backend.AppDatabase
+import com.example.sparfuchs.backend.Category
+import com.example.sparfuchs.backend.CategoryEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class CategoryViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val categoryDao = AppDatabase.getInstance(application).categoryDao()
+    val allCategories: LiveData<List<CategoryEntity>> = categoryDao.getAllCategories()
+
+    fun insert(category: CategoryEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            categoryDao.insert(category)
+        }
+    }
+
+    fun delete(category: CategoryEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            categoryDao.delete(category)
+        }
+    }
+    fun update(category: CategoryEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            categoryDao.update(category)
+        }
+    }
+
+}
--- app/src/main/java/com/example/sparfuchs/backend/AppDatabase.java
+++ app/src/main/java/com/example/sparfuchs/backend/AppDatabase.java
@@ -5,9 +5,10 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {TransactionEntity.class}, version = 1)
+@Database(entities = {TransactionEntity.class, CategoryEntity.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TransactionDao transactionDao();
+    public abstract CategoryDao categoryDao();
 
     private static volatile AppDatabase INSTANCE;
 
